d3c0fa9deeabe6175664a0c9087d67b21b9cc078
Luiz Gabriel Ferreira <luiz.gabriel.lcf@gmail.com>
2025-05-17 20:48:00 -0300

    build: configuration of api

---
+++ src/main/java/com/example/fastmarket/FastMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastMarketApplication.class, args);
+	}
+
+}

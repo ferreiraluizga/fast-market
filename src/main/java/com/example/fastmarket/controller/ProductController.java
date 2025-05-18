e870a8ee539475e7ee0c695771418fafbd33bc86
Luiz Gabriel Ferreira <luiz.gabriel.lcf@gmail.com>
2025-05-17 23:04:06 -0300

    feat: configuration of routes in ProductController

---
+++ src/main/java/com/example/fastmarket/controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.example.fastmarket.controller;
+
+import com.example.fastmarket.dto.ProductRequest;
+import com.example.fastmarket.dto.ProductResponse;
+import com.example.fastmarket.model.Product;
+import com.example.fastmarket.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@AllArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> save(@RequestBody ProductRequest dto) {
+        ProductResponse response = productService.save(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAll() {
+        List<ProductResponse> products = productService.getAll();
+        return ResponseEntity.ok(products);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> update(@PathVariable Long id, @RequestBody ProductRequest dto) {
+        ProductResponse response = productService.update(id, dto);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(Long id) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}

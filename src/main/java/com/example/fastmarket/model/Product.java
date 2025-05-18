b6d0e4a841217fa52569c229ad9b81d15d97c2ab
Luiz Gabriel Ferreira <luiz.gabriel.lcf@gmail.com>
2025-05-17 21:31:46 -0300

    feat: Product model and configuration of database

---
+++ src/main/java/com/example/fastmarket/model/Product.java
@@ -0,0 +1,35 @@
+package com.example.fastmarket.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
